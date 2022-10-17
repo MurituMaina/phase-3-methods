53f8a955b637e657ef10ccfcb89f9fab118117bc
MurituMaina <vincent.maina@student.moringaschool.com>
2022-10-17 15:33:30 +0300

    commit tree 23983cac477d6b2eadfce13edde50699371c3e54

--- methods.rb
+++ methods.rb
@@ -3,38 +3,38 @@ def greet_programmer
     puts "Hello, programmer!"
 
 end
+greet_programmer
 
-puts greet_programmer
 
 def greet name
     puts "Hello, #{name}!"
 end
-
- puts greet("Naureen")
- puts greet("Jimmy")
+greet("Naureen")
+greet("Jimmy")
 
  
 
 def greet_with_default name="programmer"
     puts "Hello, #{name}!"
 end
-puts greet_with_default("Naureen")
-puts greet_with_default()
+greet_with_default("Naureen")
+greet_with_default()
+
 
 def add num1, num2
-    sum = num1 + num2
-    puts sum
+    puts num1 + num2
     return num1 +num2
-
 end
-puts add(3,2)
+add(3,2)
+
 
 def halve number
     if number ==0 || number.class == String
-        return nil
+    p nil
+    return nil
     end
     puts number/2
     return number/2
-
 end
-    puts halve(4)
+    halve(4)
+    halve("t")
